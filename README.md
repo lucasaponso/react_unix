046dd90f496883a12286992a788bc21c1cc290ee
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-07-07 16:20:20 +1000

    mod: updated readme

--- README.md
+++ README.md
@@ -5,6 +5,26 @@ applicatiosn written in GNU C and x86 ASSM.
 
 
 ## How to Build?
+Firstly, clone the Linux kernel.
+```
+git clone git@github.com:torvalds/linux.git --depth 1
 ```
 
+Then move the react-unix-cfg into arch/x86/configs/react-unix-cfg
+```
+mv react-unix-cfg ../linux/arch/x86/configs/.
+```
+
+Then execute
+```
+make react-unix-cfg
+```
+Then run
+```
+make
+```
+
+Then to build the user-space
+```
+./build.sh
 ```
--- build.sh
+++ build.sh
@@ -1,22 +1,44 @@
+#!/bin/bash
+set -e  # Exit immediately if a command fails
 
-as sys.S -o bin/assm # compile assembly
+echo "[*] Compiling Assembly"
+as sys.S -o bin/assm
 
-gcc -static -c src/init.c -I./include -lc -o ./obj/init.o ##compile shell
-gcc -static -c src/utils.c -I./include -lc -o ./obj/utils.o ##compile util lib
+echo "[*] Compiling C source files"
+gcc -static -c src/init.c -I./include -o obj/init.o
+gcc -static -c src/utils.c -I./include -o obj/utils.o
 
-gcc -static src/ls.c bin/assm  -o bin/ls ##compile ls
-gcc -static src/cd.c -o bin/cd -lc ##compile ls
-gcc -static src/pwd.c -o bin/pwd -lc ##compile ls
+echo "[*] Building userland utilities"
+gcc -static src/ls.c bin/assm -o bin/ls
+gcc -static src/cd.c -o bin/cd
+gcc -static src/pwd.c -o bin/pwd
+gcc -static src/mkdir.c bin/assm -o bin/mkdir
 
-gcc -static src/mkdir.c bin/assm -o bin/mkdir -lc ##compile ls
+echo "[*] Linking init (shell)"
+gcc -static -o bin/init obj/init.o obj/utils.o bin/assm -z noexecstack
 
-gcc -static -o bin/init ./obj/init.o ./obj/utils.o bin/assm -lc -z noexecstack ##link shell, utils lib, assembly
-
+echo "[*] Creating initramfs"
 cd bin
-cat boot_files | cpio -H newc -o > init.cpio ##write ls, lua as exec in fs
-rm -rf init ls ##do not remove lua
+cat boot_files | cpio -H newc -o > init.cpio
 mv init.cpio ../
+cd ..
+
+echo "[*] Building ISO image from kernel"
+
+# # Ensure genisoimage or mkisofs is available
+# command -v genisoimage >/dev/null 2>&1 || {
+#     echo "genisoimage not found, trying mkisofs..."
+#     command -v mkisofs >/dev/null 2>&1 || {
+#         echo "Error: genisoimage or mkisofs is required but not found." >&2
+#         exit 1
+#     }
+#     alias genisoimage=mkisofs
+# }
 
 cd ../linux
-make isoimage FDARGS="initrd=/init.cpio" FDINITRD=/home/aponso/to_backup/linux/react_unix/init.cpio
-qemu-system-x86_64 -cdrom arch/x86/boot/image.iso
+make isoimage FDARGS="initrd=/init.cpio" FDINITRD="$(pwd)/../react_unix/init.cpio"
+
+
+echo "[*] Launching QEMU"
+qemu-system-x86_64 -cdrom arch/x86/boot/image.iso -display gtk
+
